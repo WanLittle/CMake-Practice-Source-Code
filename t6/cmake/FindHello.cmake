35dd186ace78b7e0c7cad5b1a45d9a95008ddc30
WanLittle <2621996061@qq.com>
2019-01-15 20:37:20 +0800

    feat: add t1-t6

---
+++ t6/cmake/FindHello.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 2.8)
+
+find_path(HELLO_INCLUDE_DIR hello.h ${PROJECT_SOURCE_DIR}/include/hello)
+
+find_library(HELLO_LIBRARY hello ${PROJECT_SOURCE_DIR}/lib)
+
+if(HELLO_INCLUDE_DIR AND HELLO_LIBRARY)
+	SET(HELLO_FOUND TRUE)
+endif(HELLO_INCLUDE_DIR AND HELLO_LIBRARY)
+
+if(HELLO_FOUND)
+	if(NOT HELLO_FIND_QUIETLY)
+		message(STATUS "Found Hello: ${HELLO_LIBRARY}")
+	endif(NOT HELLO_FIND_QUIETLY)
+else(HELLO_FOUND)
+	if(HELLO_FIND_REQUIRED)
+		message(FATAL_ERROR "Could not find hello library")
+	endif(HELLO_FIND_REQUIRED)
+endif(HELLO_FOUND)
